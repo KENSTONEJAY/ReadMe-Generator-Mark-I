f342261fa6db0932b1c5a4307a9026c75a739916
Ken Otuo <mrkoajr@gmail.com>
2021-03-25 11:36:43 -0400

    creating additional aspects of the README for user input

--- ReadmeGen.js
+++ ReadmeGen.js
@@ -3,21 +3,61 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 //inquirer to generate questions 
-inquirer.prompt(
+inquirer.prompt([
     
     {
         type: 'input',
-        message = 'Enter project title'
+        message="Enter project title",
         name: 'title',
         // validate property to check that the user provided a value 
         validate: (value) =>{
             if (value){
                 return true
             } else {
-                return 'Please enter a valid title'
+                return 'Please enter a valid input'
                 
             }
         }
-    }
+    },
+    {
+        type: 'input',
+        message = 'Briefly describe your app',
+        name: 'description',
+        // validate property to check that the user provided a value 
+        validate: (value) =>{
+            if (value){
+                return true
+            } else {
+                return 'Please enter a valid input'
+                
+            }
+        }
+    },
+    {
+        type: 'input',
+        message = 'Enter resources and/or technologies used',
+        name: 'installation',
+        // validate property to check that the user provided a value 
+        validate: (value) =>{
+            if (value){
+                return true
+            } else {
+                return 'Please enter a valid input'
+                
+            }
+        }
+    },
+    {
+        type: 'input',
+        message = 'Enter usage instructions',
+        name: 'usage',
+        // validate property to check that the user provided a value 
+        validate: (value) =>{
+            if (value){
+                return true
+            } else {
+                return 'Please enter a valid input'
+            }
+        }
+    },
     
-)
